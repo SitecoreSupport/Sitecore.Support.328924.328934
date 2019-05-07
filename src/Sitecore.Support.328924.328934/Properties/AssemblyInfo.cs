9c0af5229e021f57a709c7cf8bf7b9c4be7a46f2
AndreyFL <AndreyFilchenkov@gmail.com>
2019-05-07 16:59:22 +0300

    Initial

---
+++ src/Sitecore.Support.328924.328934/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.328924.328934")]
+[assembly: AssemblyProduct("Sitecore.Support.328924.328934")]
+[assembly: ComVisible(false)]
